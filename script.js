293e67c1ae0e187b83cf073524a0eead9def3df4
root <yatsumehole@naver.com>
2022-05-29 11:18:00 +0000

    pdf에 가이드라인을 추가함

--- script.js
+++ script.js
@@ -144,7 +144,6 @@ function replaceSpace(text) {
 
   // 글자들 중 빈도수가 작은 것부터 우선적으로 분류
   asciiInfoArr = sortAsciiInfoArray(asciiInfoArr);
-  console.log(asciiInfoArr);
 
   let spaceChar = detectAvailSpaceChar(asciiInfoArr, text);
   console.log(`space converted to ${spaceChar}`);
@@ -224,6 +223,7 @@ function detectAvailSpaceChar(asciiInfoArr, text) {
   }
 }
 
+// char: 221, line: 174
 function convertTextToPDF(text) {
   ttf2base64().then((result) => {
     const pdf = new jsPDF("p", "pt", "a4");
@@ -233,49 +233,57 @@ function convertTextToPDF(text) {
     pdf.addFont("UbuntuMono-R.ttf", "UbuntuMono-R", "normal");
     pdf.setFont("UbuntuMono-R");
     pdf.setFontSize(4); // hex에 대해서는 3pt로 적용되도록 수정
-    pdf.setTextColor(1); // 검은색
+    pdf.setTextColor("0"); // 검은색
 
     // 나중에 파일 종류에 따라서 다양하게 수정할 수 있도록 바꾸기
-    const marginTop = cm2point(2);
-    const marginSide = cm2point(1);
-    const pageWidth = pdf.internal.pageSize.getWidth();
-    const pageHeight = pdf.internal.pageSize.getHeight();
-    const charWidth = pdf.getTextDimensions("A").w + 0.45;
-    const charHeight = pdf.getTextDimensions("A").h * 1.1;
+    // 변수 이름도 수정 할 수도?
+    const sizeInfo = {};
+
+    sizeInfo.marginTop = cm2point(2);
+    sizeInfo.marginSide = cm2point(1);
+    sizeInfo.pageW = pdf.internal.pageSize.getWidth();
+    sizeInfo.pageH = pdf.internal.pageSize.getHeight();
+    sizeInfo.charW = pdf.getTextDimensions("A").w + 0.45;
+    sizeInfo.charH = pdf.getTextDimensions("A").h * 1.1;
 
     let pos = {
-      x: marginSide,
-      y: marginTop,
+      x: sizeInfo.marginSide,
+      y: sizeInfo.marginTop,
     };
 
-    let line = 1;
-    let char = 1;
+    let index = {
+      page: 1,
+      line: 1,
+      char: 1,
+    };
 
-    // char: 221, line: 174
     for (const c of text) {
-      if (pos.y > pageHeight - marginSide) {
-        console.log(line);
+      // 새 페이지 추가
+      if (pos.y > sizeInfo.pageH - sizeInfo.marginSide) {
         pdf.addPage();
-        pos.x = marginSide;
-        pos.y = marginTop;
-        line = 1;
+        pos.x = sizeInfo.marginSide;
+        pos.y = sizeInfo.marginTop;
+        index.line = 1;
       }
 
+      pdf.setTextColor("0.5");
+      addGuideToPage(pdf, pos, index, sizeInfo);
+      pdf.setTextColor("0");
       pdf.text(pos.x, pos.y, c);
-      pos.x += charWidth;
-      char += 1;
+      pos.x += sizeInfo.charW;
+      index.char += 1;
 
-      if (pos.x > pageWidth - marginSide) {
-        // console.log(char);
-        pos.x = marginSide;
-        pos.y += charHeight;
+      // 줄바꿈
+      if (pos.x > sizeInfo.pageW - sizeInfo.marginSide) {
+        pos.x = sizeInfo.marginSide;
+        pos.y += sizeInfo.charH;
         pos.y = roundDecimalPlace(pos.y);
-        line += 1;
-        char = 1;
+        index.line += 1;
+        index.char = 1;
       }
     }
 
-    // pdf.save("test.pdf");
+    pdf.save("test.pdf");
   });
 }
 
@@ -302,4 +310,36 @@ function cm2point(cm) {
 // 소수점 3자리까지만 정확히 숫자를 반환하도록 함
 function roundDecimalPlace(float, place = 3) {
   return Math.round(float * Math.pow(10, place)) / Math.pow(10, place);
+}
+
+function addGuideToPage(pdf, pos, index, sizeInfo) {
+  // 페이지 가이드라인 추가
+  if (index.page == 1 && index.line == 1 && index.char == 1) {
+    const pageString = "page " + index.page;
+    pdf.text(0, sizeInfo.charH, pageString);
+  }
+  // 세로 가이드라인 추가
+  if (index.char == 1) {
+    const lineString = fitNumberToGuide(index.line, (mode = "v"));
+    pdf.text(0, pos.y, lineString);
+    pdf.text(sizeInfo.pageW - sizeInfo.charW * 3, pos.y, lineString);
+  }
+  // 가로 가이드라인 추가
+  if (index.line == 1 && index.char % 10 == 0) {
+    const charString = fitNumberToGuide(index.char, (mode = "h"));
+    pdf.text(pos.x, sizeInfo.charH, charString);
+    pdf.text(pos.x, sizeInfo.pageH, charString);
+  }
+}
+
+function fitNumberToGuide(num, mode) {
+  if (mode == "v") {
+    // 세로 가이드라인, 숫자 앞에 0을 붙여서 3자리로 만들어 줌
+    return num >= 100 ? num.toString() : num >= 10 ? "0" + num : "00" + num;
+  } else if (mode == "h") {
+    // 가로 가이드라인, 숫자에서 10의 자리만 추출함
+    return num < 100
+      ? parseInt(num / 10).toString()
+      : parseInt((num % 100) / 10).toString();
+  }
 }
