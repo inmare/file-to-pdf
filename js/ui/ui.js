40fb257129f4becc6e97a2067b02d9564f04a4aa
yatsumehole <yatsumehole@naver.com>
2022-10-16 04:01:07 +0000

    addChar, removeChar함수 추가

--- js/ui/ui.js
+++ js/ui/ui.js
@@ -1,30 +1,75 @@
 import Initialize from "./initialize.js";
+import $ from "../global.js";
 
-function addChar(e) {}
+function addChar() {
+  const fromText = $("#char-from").value;
+  const toText = $("#char-to").value;
+
+  if (fromText === "" || toText === "") {
+    return alert("글자를 입력해주세요.");
+  }
+
+  const fromTd = _getAllTd(1);
+  const toTd = _getAllTd(2);
+
+  let charList = "";
+
+  for (let [idx, td] of fromTd.entries()) {
+    if (idx != 0) {
+      charList += td.innerText;
+    }
+  }
+
+  for (let [idx, td] of toTd.entries()) {
+    if (idx != 0) {
+      charList += td.innerText;
+    }
+  }
+
+  if (charList.includes(fromText) || charList.includes(toText)) {
+    return alert(
+      "이미 표에 포함된 글자입니다.\n표의 글자를 삭제하거나 다른 글자를 넣어주세요."
+    );
+  } else {
+    const fromTd = document.createElement("td");
+    const toTd = document.createElement("td");
+    const delTd = document.createElement("td");
+    const delBtn = document.createElement("i");
+    fromTd.innerText = fromText;
+    toTd.innerText = toText;
+    delTd.append(delBtn);
+    delBtn.classList.add("fa-solid", "fa-xmark");
+    delBtn.addEventListener("click", removeChar);
+
+    const fromTr = $("#char-table tr:nth-child(1)");
+    const toTr = $("#char-table tr:nth-child(2)");
+    const delTr = $("#char-table tr:nth-child(3)");
+    fromTr.append(fromTd);
+    toTr.append(toTd);
+    delTr.append(delTd);
+  }
+}
 
 function removeChar(e) {
-  const charTd = getAllTd(3);
+  const delTd = e.target.parentElement;
+  const charTd = _getAllTd(3);
 
-  const idx = Array.from(charTd).indexOf(e.target.parentElement) + 1;
-  console.log(idx);
+  const idx = Array.from(charTd).indexOf(delTd) + 1;
 
-  const targetFromTd = getTargetTd(1, idx);
-  console.log(targetFromTd);
-  const targetToTd = getTargetTd(2, idx);
+  const targetFromTd = _getTargetTd(1, idx);
+  const targetToTd = _getTargetTd(2, idx);
 
   targetFromTd.remove();
   targetToTd.remove();
-  e.target.parentElement.remove();
+  delTd.remove();
+}
 
-  function getAllTd(trIdx) {
-    return document.querySelectorAll(`#char-table tr:nth-child(${trIdx}) > td`);
-  }
+function _getAllTd(trIdx) {
+  return $(`#char-table tr:nth-child(${trIdx}) > td`);
+}
 
-  function getTargetTd(trIdx, tdIdx) {
-    return document.querySelector(
-      `#char-table tr:nth-child(${trIdx}) > td:nth-child(${tdIdx})`
-    );
-  }
+function _getTargetTd(trIdx, tdIdx) {
+  return $(`#char-table tr:nth-child(${trIdx}) > td:nth-child(${tdIdx})`);
 }
 
 export default class UI {
@@ -41,12 +86,10 @@ export default class UI {
   }
 
   static addListener() {
-    const addCharBtn = document.querySelector("#add-char");
+    const addCharBtn = $("#add-char");
     addCharBtn.addEventListener("click", addChar);
 
-    const charTd = document.querySelectorAll(
-      "#char-table tr:nth-child(3) > td i"
-    );
+    const charTd = $("#char-table tr:nth-child(3) > td i");
 
     for (let td of charTd) {
       td.addEventListener("click", removeChar);
