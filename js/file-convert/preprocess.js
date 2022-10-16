36e10fdb94fd70275a35d8d4ba64942e7be7177b
yatsumehole <yatsumehole@naver.com>
2022-10-16 11:49:35 +0000

    changeSetting함수 및 관련 함수 추가

--- js/file-convert/preprocess.js
+++ js/file-convert/preprocess.js
@@ -1,12 +1,12 @@
+import Setting from "../setting/setting.js";
+import Util from "../ui/util.js";
 import UI from "../ui/ui.js";
 import $ from "../global.js";
 
 export default class Preprocess {
   static initialize() {
-    this.fileInput = $("#input-btn");
-    this.fileConvert = $("#convert-btn");
-    this.fileInput.addEventListener("change", this.getFileInfo);
-    this.fileConvert.addEventListener("click", this.convertFile.bind(this));
+    const fileInput = $("#input-btn");
+    fileInput.addEventListener("change", this.getFileInfo);
   }
 
   static getFileInfo(e) {
@@ -25,14 +25,110 @@ export default class Preprocess {
     UI.displayFileInfo(name, sizeKb);
   }
 
-  static async convertFile() {
-    if (!this.fileInput.files.length) {
-      return alert("파일이 없습니다!");
+  static changeSetting() {
+    const inputType = ["select", "text", "checkbox", "table"];
+    for (let type of inputType) {
+      switch (type) {
+        case "select":
+          getSelectSetting();
+          break;
+        case "text":
+          getTextSetting();
+          break;
+        case "checkbox":
+          getCheckboxSetting();
+          break;
+        case "table":
+          getTableSetting();
+          break;
+        default:
+          break;
+      }
     }
 
-    const file = this.fileInput.files[0];
-    const text = await this.readTextFromFile(file);
-    const hasNonAscii = this.checkAscii(text);``
+    function getSelectSetting() {
+      const selectElem = $("#setting-section select");
+      for (let elem of selectElem) {
+        const keyName = _dashToCamelCase(elem.id);
+        Setting[keyName].default = elem.value;
+      }
+    }
+
+    function getTextSetting() {
+      const textElem = $("#setting-section input[type='text']");
+      for (let elem of textElem) {
+        if (elem.id == "char-from" || elem.id == "char-to") {
+          continue;
+        }
+        const value = elem.value;
+        const isValid = _checkTextValue(value);
+        if (isValid) {
+          const keyName = _dashToCamelCase(elem.id);
+          Setting[keyName].default = parseFloat(value);
+        }
+      }
+    }
+
+    function getCheckboxSetting() {
+      const checkboxElem = $("#setting-section input[type='checkbox']");
+      for (let elem of checkboxElem) {
+        const checked = elem.checked;
+        const keyName = _dashToCamelCase(elem.id);
+        Setting[keyName].default = checked;
+      }
+    }
+
+    function getTableSetting() {
+      const fromTd = Util.getAllTd(1);
+      const toTd = Util.getAllTd(2);
+      let fromArray = [];
+      let toArray = [];
+
+      for (let [idx, td] of fromTd.entries()) {
+        if (idx != 0) {
+          fromArray.push(td.innerText);
+        }
+      }
+
+      for (let [idx, td] of toTd.entries()) {
+        if (idx != 0) {
+          toArray.push(td.innerText);
+        }
+      }
+
+      Setting.charTable.from = fromArray;
+      Setting.charTable.to = toArray;
+    }
+
+    function _dashToCamelCase(text) {
+      const regex = /-./g;
+      const camelCase = text.replace(regex, (match) => {
+        return match.slice(1).toUpperCase();
+      });
+      return camelCase;
+    }
+
+    function _checkTextValue(value) {
+      if (value === "") {
+        return false;
+      } else {
+        const isNum = _checkTextIsNumber(value);
+        return isNum;
+      }
+    }
+
+    function _checkTextIsNumber(text) {
+      const intRegex = /^\d*$/;
+      const floatRegex = /^\d+\.\d+$/;
+      const intMatch = text.match(intRegex);
+      const floatMatch = text.match(floatRegex);
+
+      if (intMatch || floatMatch) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 
   static readTextFromFile(file) {
