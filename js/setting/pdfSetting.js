9c9cc917200f326ba12936d6408bb7170257015d
yatsumehole <yatsumehole@naver.com>
2022-10-23 10:07:03 +0000

    설정 항목 추가

--- js/setting/pdfSetting.js
+++ js/setting/pdfSetting.js
@@ -4,6 +4,15 @@ const pdfSetting = {
     fileNameLength: 2,
     lastLineCharLength: 3,
   },
+  metadata: {
+    fileName: null,
+    fileSize: null,
+    fileNameUnicode: null,
+    convertTypeTable: ["Ascii", "Hex", "Unicode", "Base64"],
+    convertTypeDec: null,
+    fileNameLength: null,
+    dummyTextLength: null,
+  },
 };
 
 export default pdfSetting;
