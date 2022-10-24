d2a06ccc0e43bdb40842f16bde0be23c404a9de6
yatsumehole <yatsumehole@naver.com>
2022-10-24 12:21:14 +0000

    createPage함수 수정

--- js/pdfGenerate/pdf.js
+++ js/pdfGenerate/pdf.js
@@ -10,22 +10,25 @@ export default class PDF {
     await Font.addFont(doc, Setting.fontType.default);
     // 메인 텍스트용 메타 데이터 추가
     Metadata.setMetadata(doc, file, text);
+    console.table(pdfSetting);
     const textWithData = Metadata.comebineTextAndData(text);
     // const makeFirstPageGuide = Setting.firstPageGuide.default;
     this.createPage(doc, textWithData);
+
+    // doc.save("test.pdf");
   }
 
   static async createRandomTextPDF() {
     // 랜덤한 텍스트로 구성된 pdf만들기
   }
 
-  static createFirstPageGuide(doc, metadata, charInfo) {
+  static createFirstPageGuide(doc) {
     doc.addPage();
   }
 
   static createPage(doc, text) {
     const font = Setting.fontType.default;
-    const fontSize = Setting.fontType.default;
+    const fontSize = Setting.fontSize.default;
     const kerning = Setting.kerning.default;
     const lineHeight = Setting.lineHeight.default;
 
@@ -36,19 +39,21 @@ export default class PDF {
 
     doc.setFont(font);
     doc.setFontSize(fontSize);
+    doc.setTextColor("0");
 
-    const charInfo = pdfSetting.metadata.charInfo;
-    const textPerPage = charInfo.textPerPage;
-    const wholePage = Math.ceil(textPerPage / text.length);
+    const textPerPage = pdfSetting.charInfo.textPerPage;
+    const wholePage = Math.ceil(text.length / textPerPage);
+
+    const x = Setting.mLeft.pt;
+    const y = Setting.mTop.pt;
 
     for (let i = 0; i < wholePage; i++) {
-      doc.addPage();
       const startIdx = textPerPage * i;
       let endIdx = textPerPage * (i + 1);
       endIdx = text.length > endIdx ? text.length : endIdx;
 
       const pageText = text.slice(startIdx, endIdx);
-      
+      doc.text(pageText, x, y, options);
     }
   }
 }
