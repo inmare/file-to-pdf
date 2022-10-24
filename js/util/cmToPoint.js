196a87c1ac13193f218eefd357786f006057104c
yatsumehole <yatsumehole@naver.com>
2022-10-24 12:19:58 +0000

    부동 소수점 오류 수정

--- js/util/cmToPoint.js
+++ js/util/cmToPoint.js
@@ -1,5 +1,7 @@
 function cmToPoint(cm) {
-  return cm * 28.346;
+  // 부동 소수점 오차를 해결하기 위해 소수점 다섯째 자리에서 반올림함
+  const point = Math.round(cm * 28.346 * 10000) / 10000;
+  return point;
 }
 
 export default cmToPoint;
