ea260717143a26c9fa792ebf2eeed3a399b48f0b
yatsumehole <yatsumehole@naver.com>
2022-10-24 11:02:00 +0000

    설정 객체 구조 변경

--- js/setting/pdfSetting.js
+++ js/setting/pdfSetting.js
@@ -1,47 +1,48 @@
 const pdfSetting = {
-  metadataLength: {
+  dataLength: {
     convertTypeDec: 1,
     fileNameLength: 2,
     lastLineLength: 3,
   },
-  metadata: {
-    file: {
-      name: null,
-      size: null,
-    },
-    text: {
-      fileNameUnicode: null,
-      convertTypeTable: ["Ascii", "Hex", "Unicode", "Base64"],
-      convertTypeDec: {
-        value: null,
-        get str() {
-          const valueStr = String(value);
-          const maxLength = pdfSetting.metadataLength.convertTypeDec;
-          return valueStr.padStart(maxLength, "0");
-        },
-      },
-      fileNameLength: {
-        value: null,
-        get str() {
-          const valueStr = String(value);
-          const maxLength = pdfSetting.metadataLength.fileNameLength;
-          return valueStr.padStart(maxLength, "0");
-        },
-      },
-      lastLineLength: {
-        value: null,
-        get str() {
-          const valueStr = String(value);
-          const maxLength = pdfSetting.metadataLength.lastLineLength;
-          return valueStr.padStart(maxLength, "0");
-        },
+  file: {
+    name: null,
+    size: null,
+  },
+  text: {
+    fileNameUnicode: null,
+    convertTypeTable: ["Ascii", "Hex", "Unicode", "Base64"],
+    convertTypeDec: {
+      value: null,
+      get str() {
+        const valueStr = String(value);
+        const maxLength = pdfSetting.dataLength.convertTypeDec;
+        return valueStr.padStart(maxLength, "0");
       },
     },
-    charInfo: {
-      charPerLine: null,
-      linePerPage: null,
-      textPerPage: null,
+    fileNameLength: {
+      value: null,
+      get str() {
+        const valueStr = String(value);
+        const maxLength = pdfSetting.dataLength.fileNameLength;
+        return valueStr.padStart(maxLength, "0");
+      },
     },
+    lastLineLength: {
+      value: null,
+      get str() {
+        const valueStr = String(value);
+        const maxLength = pdfSetting.dataLength.lastLineLength;
+        return valueStr.padStart(maxLength, "0");
+      },
+    },
+  },
+  charInfo: {
+    charPerLine: null,
+    linePerPage: null,
+    textPerPage: null,
+    // 후에 정보 추가하기
+    charWidth: null,
+    charHeight: null,
   },
   firstPage: {
     font: "Pretendard-Medium",
