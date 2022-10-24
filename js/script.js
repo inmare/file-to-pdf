fd02a5fcecad2e3b4e0f3cd88f9a1597d9266c65
yatsumehole <yatsumehole@naver.com>
2022-10-24 11:17:26 +0000

    $ 글로벌 변수 선언에 따른 코드 수정

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 // import { textToPDF } from "./pdf-conversion.js";
 // import { zeroToO } from "./type-conversion.js";
 
+import $ from "./util/global.js";
 import UI from "./ui/ui.js";
 import Convert from "./fileConvert/convert.js";
 import Preprocess from "./fileConvert/preprocess.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   window.jsPDF = window.jspdf.jsPDF;
+  window.$ = $;
   UI.initialize();
   Convert.initialize();
   Preprocess.initialize();
