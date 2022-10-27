636ec4ae66f406cbe36eedf5682d953ee05031a6
yatsumehole <yatsumehole@naver.com>
2022-10-27 10:53:35 +0000

    각 table 생성 관련 코드 삭제, 랜덤 텍스트 생성시 대응 코드 추가

--- js/pdfGenerate/pdf.js
+++ js/pdfGenerate/pdf.js
@@ -6,13 +6,24 @@ import Font from "./font.js";
 import Text from "../util/text.js";
 
 export default class PDF {
-  static async createPDF(text, file) {
+  static async createPDF(text, file, isRandomText) {
     const doc = new jsPDF("p", "pt", "a4");
     // 메인 텍스트용 폰트 추가
     await Font.addFont(doc, Setting.fontType.default);
     // 메인 텍스트용 메타 데이터 추가
-    Metadata.setMetadata(doc, file, text);
-    const textWithData = Metadata.comebineTextAndData(text);
+    let processedText;
+    let outputFileName;
+    if (!isRandomText) {
+      Metadata.setMetadata(doc, text, file, isRandomText);
+      processedText = Metadata.comebineTextAndData(text);
+      outputFileName = `${file.name}.pdf`;
+    } else {
+      Metadata.setMetadata(doc, null, null, isRandomText);
+      processedText = text;
+      const textType = Setting.randomTextType;
+      outputFileName = `Random${textType}.pdf`;
+    }
+
     const makeFirstPageGuide = Setting.firstPageGuide.default;
 
     if (makeFirstPageGuide) {
@@ -22,18 +33,14 @@ export default class PDF {
       doc.addPage();
     }
 
-    this.createPage(doc, textWithData);
+    this.createPage(doc, processedText);
 
     doc.output("dataurlnewwindow", {
-      filename: `${file.name}.pdf`,
+      filename: outputFileName,
     });
   }
 
-  static async createRandomTextPDF() {
-    // 랜덤한 텍스트로 구성된 pdf만들기
-  }
-
-  static createFirstPageGuide(doc) {
+  static createFirstPageGuide(doc, isRandomText) {
     const firstPageFont = pdfSetting.firstPage.fontType;
     const firstPageFontSize = pdfSetting.firstPage.fontSize;
     const margin = pdfSetting.firstPage.margin.pt;
@@ -47,78 +54,39 @@ export default class PDF {
 
     doc.text("PDF 정보", margin, totalHeight);
     addLineBreak(1);
-    addInfoTable();
 
+    const infoTable = firstPageTable.infoTable(isRandomText);
+    setTableConfig(infoTable);
+    doc.autoTable(infoTable);
     addLineBreak(2);
 
     doc.text("변경된 텍스트", margin, totalHeight);
     addLineBreak(1);
-    addCharListTable();
 
+    const charListTable = firstPageTable.charListTable();
+    setTableConfig(charListTable);
+    doc.autoTable(charListTable);
     addLineBreak(2);
 
-    doc.text("메타데이터", margin, totalHeight);
-    addLineBreak(1);
-    addMetadataTable();
+    if (isRandomText) {
+      doc.text("메타데이터", margin, totalHeight);
+      addLineBreak(1);
+
+      const meatdataTable = firstPageTable.meatdataTable();
+      setTableConfig(meatdataTable);
+      doc.autoTable(meatdataTable);
+    }
 
     function addLineBreak(lineNum) {
       totalHeight += textHeight * lineNum;
     }
 
-    function addMetadataTable() {
-      const metadataTable = firstPageTable.vertical;
-      metadataTable.body = [
-        {
-          title: "변경 모드",
-          data: pdfSetting.text.convertTypeDec.str,
-        },
-        {
-          title: "파일 이름(유니코드)의 길이",
-          data: pdfSetting.text.fileNameLength.str,
-        },
-        {
-          title: "마지막 줄의 실제 길이",
-          data: pdfSetting.text.lastLineLength.str,
-        },
-      ];
-      metadataTable.startY = totalHeight;
-      metadataTable.didDrawPage = function (data) {
+    function setTableConfig(tableConfig) {
+      tableConfig.startY = totalHeight;
+      tableConfig.didDrawPage = function (data) {
         // 표가 그려진 후 cursor y좌표를 totalHeight에 할당하기
         totalHeight = data.cursor.y;
       };
-      doc.autoTable(metadataTable);
-    }
-
-    function addCharListTable() {
-      const charListTable = firstPageTable.horizontal;
-      charListTable.head = [["글자", ...Setting.charTable.from]];
-      charListTable.body = [
-        ["변환된 글자", ...Setting.charTable.to],
-        ["유니코드", ...Setting.charTable.toUnicode],
-      ];
-      charListTable.startY = totalHeight;
-      charListTable.didDrawPage = function (data) {
-        totalHeight = data.cursor.y;
-      };
-      doc.autoTable(charListTable);
-    }
-
-    function addInfoTable() {
-      const infoTable = firstPageTable.vertical;
-      infoTable.body = [
-        { title: "파일 이름", data: pdfSetting.file.name },
-        { title: "파일 크기", data: pdfSetting.file.size },
-        { title: "변경 모드", data: Setting.convertType.default },
-        { title: "줄 당 글자 수", data: pdfSetting.charInfo.charPerLine },
-        { title: "페이지 당 줄 수", data: pdfSetting.charInfo.linePerPage },
-        { title: "페이지 당 글자 수", data: pdfSetting.charInfo.textPerPage },
-      ];
-      infoTable.startY = totalHeight;
-      infoTable.didDrawPage = function (data) {
-        // 표가 그려진 후 cursor y좌표를 totalHeight에 할당하기
-        totalHeight = data.cursor.y;
-      };
-      doc.autoTable(infoTable);
     }
   }
 
