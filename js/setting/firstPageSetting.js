baa447fdc58d465f17b847977fbccb56ebc02e2f
yatsumehole <yatsumehole@naver.com>
2022-10-27 10:53:07 +0000

    각 table에 관련한 설정들 추가

--- js/setting/firstPageSetting.js
+++ js/setting/firstPageSetting.js
@@ -17,7 +17,7 @@ const _commonStyle = {
 };
 
 const firstPageTable = {
-  vertical: {
+  _vertical: {
     // 보여줄 표의 종류에 따라 body에 추가하는 값이 달라짐
     // startY 및 didDrawPage 함수는 설정을 적용하는 함수 내부에서 유동적으로 결정함
     columns: [
@@ -50,7 +50,7 @@ const firstPageTable = {
     tableWidth: "wrap",
     margin: { left: pdfSetting.firstPage.margin.pt },
   },
-  horizontal: {
+  _horizontal: {
     headStyles: {
       fillColor: _headerColor,
     },
@@ -65,6 +65,58 @@ const firstPageTable = {
     },
     tableWidth: "wrap",
     margin: { left: pdfSetting.firstPage.margin.pt },
+  },
+  infoTable: function (makeRandomText) {
+    const config = this._vertical;
+    let body = [
+      { title: "줄 당 글자 수", data: pdfSetting.charInfo.charPerLine },
+      { title: "페이지 당 줄 수", data: pdfSetting.charInfo.linePerPage },
+      { title: "페이지 당 글자 수", data: pdfSetting.charInfo.textPerPage },
+    ];
+    if (!makeRandomText) {
+      body = [
+        { title: "파일 이름", data: pdfSetting.file.name },
+        { title: "파일 크기", data: pdfSetting.file.size },
+        { title: "변경 모드", data: Setting.convertType.default },
+        ...body,
+      ];
+    }
+
+    config.body = body;
+    return config;
+  },
+  charListTable: function () {
+    const config = this._horizontal;
+    const head = [["글자", ...Setting.charTable.from]];
+    const body = [
+      ["변환된 글자", ...Setting.charTable.to],
+      ["유니코드", ...Setting.charTable.toUnicode],
+    ];
+
+    config.head = head;
+    config.body = body;
+
+    return config;
+  },
+  meatdataTable: function () {
+    const config = this._vertical;
+    const body = [
+      {
+        title: "변경 모드",
+        data: pdfSetting.text.convertTypeDec.str,
+      },
+      {
+        title: "파일 이름(유니코드)의 길이",
+        data: pdfSetting.text.fileNameLength.str,
+      },
+      {
+        title: "마지막 줄의 실제 길이",
+        data: pdfSetting.text.lastLineLength.str,
+      },
+    ];
+
+    config.body = body;
+    return config;
   },
 };
 
