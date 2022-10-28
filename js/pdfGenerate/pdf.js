841eda842d1d0dee34326d1614689e6c23f89be6
yatsumehole <yatsumehole@naver.com>
2022-10-28 11:19:25 +0000

    코드 리팩터링 및 createLineGuide함수 추가

--- js/pdfGenerate/pdf.js
+++ js/pdfGenerate/pdf.js
@@ -8,9 +8,10 @@ import Text from "../util/text.js";
 export default class PDF {
   static async createPDF(text, file, isRandomText) {
     const doc = new jsPDF("p", "pt", "a4");
-    // 메인 텍스트용 폰트 추가
+
     await Font.addFont(doc, Setting.fontType.default);
-    // 메인 텍스트용 메타 데이터 추가
+    await Font.addFont(doc, pdfSetting.guideFont);
+
     let processedText;
     let outputFileName;
     if (!isRandomText) {
@@ -27,13 +28,11 @@ export default class PDF {
     const makeFirstPageGuide = Setting.firstPageGuide.default;
 
     if (makeFirstPageGuide) {
-      const firstPageFont = pdfSetting.firstPage.fontType;
-      await Font.addFont(doc, firstPageFont);
       this.createFirstPageGuide(doc, isRandomText);
       doc.addPage();
     }
 
-    this.createPage(doc, processedText);
+    this.createPage(doc, processedText, isRandomText);
 
     doc.output("dataurlnewwindow", {
       filename: outputFileName,
@@ -41,7 +40,7 @@ export default class PDF {
   }
 
   static createFirstPageGuide(doc, isRandomText) {
-    const firstPageFont = pdfSetting.firstPage.fontType;
+    const firstPageFont = pdfSetting.guideFont;
     const firstPageFontSize = pdfSetting.firstPage.fontSize;
     const margin = pdfSetting.firstPage.margin.pt;
 
@@ -52,36 +51,39 @@ export default class PDF {
     const textHeight = doc.getTextDimensions("A").h * doc.getLineHeightFactor();
     let totalHeight = margin;
 
-    doc.text("PDF 정보", margin, totalHeight);
-    addLineBreak(1);
+    addText("PDF 정보");
 
     const infoTable = firstPageTable.infoTable(isRandomText);
-    setTableConfig(infoTable);
-    doc.autoTable(infoTable);
-    addLineBreak(2);
+    addTable(infoTable);
 
-    doc.text("변경된 텍스트", margin, totalHeight);
-    addLineBreak(1);
+    addText("변경된 텍스트");
 
     const charListTable = firstPageTable.charListTable();
-    setTableConfig(charListTable);
-    doc.autoTable(charListTable);
-    addLineBreak(2);
+    addTable(charListTable);
 
     if (!isRandomText) {
-      doc.text("메타데이터", margin, totalHeight);
-      addLineBreak(1);
+      addText("메타데이터");
 
       const meatdataTable = firstPageTable.meatdataTable();
-      setTableConfig(meatdataTable);
-      doc.autoTable(meatdataTable);
+      addTable(meatdataTable);
     }
 
-    function addLineBreak(lineNum) {
+    function addText(text) {
+      doc.text(text, margin, totalHeight);
+      _addLineBreak(1);
+    }
+
+    function addTable(tableConfig) {
+      _setTableConfig(tableConfig);
+      doc.autoTable(tableConfig);
+      _addLineBreak(2);
+    }
+
+    function _addLineBreak(lineNum) {
       totalHeight += textHeight * lineNum;
     }
 
-    function setTableConfig(tableConfig) {
+    function _setTableConfig(tableConfig) {
       tableConfig.startY = totalHeight;
       tableConfig.didDrawPage = function (data) {
         // 표가 그려진 후 cursor y좌표를 totalHeight에 할당하기
@@ -90,7 +92,7 @@ export default class PDF {
     }
   }
 
-  static createPage(doc, text) {
+  static createPage(doc, text, isRandomText) {
     const font = Setting.fontType.default;
     const fontSize = Setting.fontSize.default;
     const kerning = Setting.kerning.default;
@@ -119,7 +121,71 @@ export default class PDF {
 
       const pageText = text.slice(startIdx, endIdx);
       const linebreakPageText = Text.addLinebreak(pageText);
+      console.log(linebreakPageText[21][2], linebreakPageText[21][3]);
       doc.text(linebreakPageText, x, y, options);
+
+      // 가이드 라인 생성
+      const makeLineGuide = Setting.lineGuide.default;
+      if (makeLineGuide) {
+        this.createLineGuide(doc, pageText, i + 1, isRandomText);
+      }
     }
+  }
+
+  static createLineGuide(doc, pageText, pageNum, isRandomText) {
+    doc.setTextColor("0.5");
+
+    const charLength = pdfSetting.charInfo.charPerLine;
+    const lineLength = pageText.length / charLength;
+
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+
+    const textWidth = pdfSetting.charInfo.charWidth;
+    const textHeight = pdfSetting.charInfo.charHeight;
+
+    const lineGuideMargin = pdfSetting.lineGuide.margin.pt;
+    const mTop = Setting.mTop.pt;
+    const mLeft = Setting.mLeft.pt;
+
+    for (let i = 1; i * 10 <= lineLength; i++) {
+      const lineNum = i * 10;
+      const lineNumStr = Text.getTensFromDec(lineNum);
+      const xLeft = lineGuideMargin;
+      const xRight = pageWidth - lineGuideMargin;
+      const y = mTop + textHeight * lineNum;
+
+      doc.text(lineNumStr, xLeft, y);
+      doc.text(lineNumStr, xRight, y);
+    }
+
+    for (let i = 1; i * 10 <= charLength; i++) {
+      const charNum = i * 10;
+      const charNumStr = Text.getTensFromDec(charNum);
+      const x = mLeft + textWidth * charNum;
+      const yTop = lineGuideMargin;
+      const yBottom = pageHeight - lineGuideMargin;
+
+      doc.text(charNumStr, x, yTop);
+      doc.text(charNumStr, x, yBottom);
+    }
+
+    doc.text(`page ${pageNum}`, lineGuideMargin, lineGuideMargin);
+
+    // 파일 변환시 파일 이름도 추가
+    if (!isRandomText) {
+      // 파일 이름에는 영어가 아닌 글자가 포함될 수도 있기 때문에 guideFont로 이름을 표시함
+      const fileNameFont = pdfSetting.guideFont;
+      doc.setFont(fileNameFont);
+      doc.text(
+        pdfSetting.file.name,
+        lineGuideMargin,
+        lineGuideMargin + textHeight
+      );
+      // 파일 이름 표시 후 다시 원래 페이지 폰트로 되돌리기
+      doc.setFont(Setting.fontType.default);
+    }
+
+    doc.setTextColor("0");
   }
 }
